009fbe7cc18220e0c2fb38a326eefa33461b1a1f
ohendra <utomo.hendra@payfazz.com>
2018-04-01 11:57:42 +0700

    MOVED route from server.js to separate Files.
    this will be easier to maintained the route file.

--- server.js
+++ server.js
@@ -4,26 +4,11 @@ const express = require('express')
 // execute express
 const app = express()
 
-// fungsi helloWorld
-const helloWorld = (request, respond) =>
-  respond.send('Hello World!')
+// import routes dari config/routes
+const router = require('./config/routes')
 
-// fungsi user get name dan address
-const userFunction = (req, res) => {
-  const { name, address } = req.params
-
-  res
-    .status(200)
-    .json({
-      name,
-      address,
-    })
-  }
-
-// get root request
-app.get('/', helloWorld)
-
-app.get('/:name/:address', userFunction)
+// add router to express
+app.use('/', router)
 
 // set express to running on port 3000
 app.listen(3000)

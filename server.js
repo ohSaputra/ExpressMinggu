93c7079a0a36a1b0323ed40f35fec204f5334089
ohendra <utomo.hendra@payfazz.com>
2018-04-01 10:49:54 +0700

    ADDED nodemon to dev depedency
    Nodemon will be used in dev stage

--- server.js
+++ server.js
@@ -4,20 +4,26 @@ const express = require('express')
 // execute express
 const app = express()
 
-// get root request
-app.get('/', (request, respond) =>
-  respond.send('Hello World!') )
+// fungsi helloWorld
+const helloWorld = (request, respond) =>
+respond.send('Hello World!')
 
-app.get('/:name/:address', (req, res) => {
+// fungsi user get name dan address
+const userFunction = (req, res) => {
   const { name, address } = req.params
-  
+
   res
     .status(200)
     .json({
       name,
       address,
     })
-})
+  }
+
+// get root request
+app.get('/', helloWorld)
+
+app.get('/:name/:address', userFunction)
 
 // set express to running on port 3000
 app.listen(3000)
